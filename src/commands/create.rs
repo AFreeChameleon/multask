7ebcab771f47ea96fa380a751be56a1c15ba4db9
AFreeChameleon <ben.evans@chameleo.dev>
2024-11-02 13:07:25 +0000

    Made commands on create not need quotes

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -16,43 +16,42 @@ pub fn run() -> Result<(), MultErrorTuple> {
     let args = env::args();
     let parsed_args = parse_args(&args.collect::<Vec<String>>()[2..], &FLAGS, true)?;
     let flags = get_fork_flag_values(&parsed_args)?;
-    for arg in parsed_args.values.iter() {
-        let mut new_task_id = 0;
-        let mut tasks: Vec<Task> = TaskManager::get_tasks()?;
-        if let Some(last_task) = tasks.last() {
-            new_task_id = last_task.id + 1;
-        }
-        tasks.push(Task { id: new_task_id });
-        print_info("Running command...");
-        let files = TaskManager::generate_task_files(new_task_id, &tasks);
-        #[cfg(target_family = "unix")]
-        {
-            use home::home_dir;
-            use mult_lib::command::CommandData;
-            use mult_lib::unix::fork;
-            let current_dir = match env::current_dir() {
-                Ok(val) => val,
-                Err(_) => home_dir().unwrap(),
-            };
-            fork::run_daemon(
-                files,
-                CommandData {
-                    pid: 0,
-                    command: arg.to_string(),
-                    dir: current_dir.display().to_string(),
-                    name: String::new(),
-                    starttime: 0,
-                },
-                flags.clone(),
-            )?;
-        }
-        #[cfg(target_family = "windows")]
-        {
-            use mult_lib::windows::fork;
-            fork::run_daemon(files, arg.to_string(), &flags, new_task_id)?;
-        }
-
-        print_success(&format!("Process {} created.", new_task_id));
+    let command = parsed_args.values.join(" ");
+    let mut new_task_id = 0;
+    let mut tasks: Vec<Task> = TaskManager::get_tasks()?;
+    if let Some(last_task) = tasks.last() {
+        new_task_id = last_task.id + 1;
     }
+    tasks.push(Task { id: new_task_id });
+    print_info("Running command...");
+    let files = TaskManager::generate_task_files(new_task_id, &tasks);
+    #[cfg(target_family = "unix")]
+    {
+        use home::home_dir;
+        use mult_lib::command::CommandData;
+        use mult_lib::unix::fork;
+        let current_dir = match env::current_dir() {
+            Ok(val) => val,
+            Err(_) => home_dir().unwrap(),
+        };
+        fork::run_daemon(
+            files,
+            CommandData {
+                pid: 0,
+                command,
+                dir: current_dir.display().to_string(),
+                name: String::new(),
+                starttime: 0,
+            },
+            flags.clone(),
+        )?;
+    }
+    #[cfg(target_family = "windows")]
+    {
+        use mult_lib::windows::fork;
+        fork::run_daemon(files, arg.to_string(), &flags, new_task_id)?;
+    }
+
+    print_success(&format!("Process {} created.", new_task_id));
     Ok(())
 }
